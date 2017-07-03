f11f655f45e89b4dd71b68c2e5ea4509d0c80f62
alezh <watman_liu@163.com>
2017-07-03 16:10:59 +0800

    test chart
    open source  : macgile/WPF.Metro.UI.Charts.R3

---
+++ GAMandelkowMetroChartsTestApplication/Models/GraphSeriesInformation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+
+namespace GAMandelkowMetroChartsTestApplication.Models
+{
+    /// <summary>
+    /// 图表信息模型
+    /// </summary>
+    public class GraphSeriesInformation : ViewModelBase
+    {
+        private string _seriesDisplayName;
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string seriesDisplayName
+        {
+            get
+            {
+                return _seriesDisplayName;
+            }
+            set
+            {
+                if (_seriesDisplayName != value)
+                {
+                    _seriesDisplayName = value;
+                    RaisePropertyChanged("seriesDisplayName");//定义键
+                }
+            }
+        }
+        /// <summary>
+        /// 集合数据
+        /// </summary>
+        public ObservableCollection<GraphSeriesDataPoint> Items { get; set; }
+        /// <summary>
+        /// 新建集合数据
+        /// </summary>
+        public GraphSeriesInformation()
+        {
+            Items = new ObservableCollection<GraphSeriesDataPoint>();
+        }
+    }
+}
